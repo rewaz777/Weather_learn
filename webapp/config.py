76fb709cbad29c294352d70f5e194817ea36e811
rewaz777 <fda77777@gmail.com>
2019-12-22 19:14:49 +0200

    Db Flask-SQLAlchemy

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 WEATHER_DEFAULT_CITY = "Kiev, Ukraine"
 WEATHER_API_KEY = "815c91a1e5594b43ab9141844191812"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 
